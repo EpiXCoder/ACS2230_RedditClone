971c9513f68bc6505098fa4d981e27331f265a54
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-04-12 18:54:26 -0400

    Users can view a list of posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,41 @@
 const Post = require('../models/post');
 
 module.exports = (app) => {
-    // CREATE
-    app.post('/posts/new', (req, res) => {
-        // INSTANTIATE INSTANCE OF POST MODEL
-        const post = new Post(req.body);
 
-        // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
-        post.save(() => res.redirect('/'));
-    });
-  };
+  // INDEX
+  app.get('/', async (req, res) => {
+    try {
+      const posts = await Post.find({}).lean();
+      return res.render('posts-index', { posts });
+    } catch (err) {
+      console.log(err.message);
+    }
+  })
+
+  // NEW
+  app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
+  })
+
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+    // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
+    post.save()
+      .then(() => {
+        res.redirect('/')
+      })
+      .catch(err => console.log(err))
+  });
+
+  // SHOW post using :id
+  app.get('/posts/:id', async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id).lean()
+      return res.render('posts-show', { post });
+    } catch {
+      console.log(err.message);
+    }   
+  });
+};
