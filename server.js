4ca99702852d37955d0e72e68cdc27d27ffa1f2d
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-04-12 14:02:49 -0400

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,14 +1,48 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
+// Require stuff here
+// require('dotenv').config();
+const express = require('express');
+// import express from 'express';
+const handlebars = require('express-handlebars');
+// import handlebars from 'express-handlebars';
 
 const app = express();
+app.use(express.static('public'));
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+// Set db
+require('./data/reddit-db.js');
 
-app.get('/', (req, res) => {
-    res.render('home');
+// Middleware
+
+const hbs = handlebars.create({
+    helpers: {}
 });
 
-app.listen(3000);
+// App config
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Require controllers
+require('./controllers/posts')(app);
+// require('./controllers/users')(app);
+
+// Routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/posts/new', (req, res) => {
+  console.log("posting!")
+  res.render('posts-new');
+});
+
+app.get('/users/new', (req, res) => {
+  console.log("making a user!")
+  res.render('users-new');
+});
+
+app.listen(3000, () => {
+  console.log('Reddit Clone listening on port localhost:3000!');
+});
