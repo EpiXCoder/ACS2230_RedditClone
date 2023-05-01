7bc1851ad5154b94332509b1ee12aa20b524e86b
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-05-01 02:09:05 -0400

    Implemented nested comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,25 +3,14 @@ const Comment = require('../models/comment');
 
 
 module.exports = (app) => {
-    // CREATE Comment
     app.post('/posts/:postId/comments', async (req, res) => {
     try {
-        // INSTANTIATE INSTANCE OF MODEL
         const comment = new Comment(req.body);
         comment.author = req.user._id;
-        // SAVE INSTANCE OF Comment MODEL TO DB
         await comment.save();
-        
-        // Find post by ID
         const [post] = await Promise.all([Post.findById(req.params.postId), ]);
-
-        // Add comment to post comments array and save
         post.comments.unshift(comment);
         await post.save();
-        // return Promise.all([
-        //     post.save(),
-        //   ]);
-
         // Redirect
         res.redirect(`/posts/${req.params.postId}`);
     } catch (err) {
--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(checkAuth);
 require('./controllers/posts')(app);
 require('./controllers/comments.js')(app);
 require('./controllers/auth.js')(app);
+require('./controllers/replies.js')(app);
 // require('./controllers/users')(app);
 
 // Routes
